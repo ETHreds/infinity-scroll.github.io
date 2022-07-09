1d5e33c0dc36c652d2e497f99cdee9e83912eb75
ETHreds <jcmaglaw@gmail.com>
2022-07-09 19:13:25 +0300

    Implemented DRY coding
    Deleted repetetive code.Added a function
    dont understand it yet well.Revisit

--- script.js
+++ script.js
@@ -8,21 +8,38 @@ const count = 10;
 const apiKey = "J5lzh6qG_3gBZhlcvuU-UDk-duXJFmGrrl_aYDwWNJM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper functon to set attributes\
+
+function setAttributes(element, attributes) {
+
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
 // Display photos and put into DOM
 function displayPhotos() {
     // get info in photoArray and populate
     photosArray.forEach((photo) => {
         // createthe <a> element to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href:photo.links.html,
+            target: '_blank',
+        });
 
         // image to be be the thing inside the link
 
         const pic = document.createElement('img');
-        pic.setAttribute('src', photo.urls.regular);
-        pic.setAttribute('alt', photo.alt_description);
-        pic.setAttribute('title', photo.alt_description);
+        // pic.setAttribute('src', photo.urls.regular);
+        // pic.setAttribute('alt', photo.alt_description);
+        // pic.setAttribute('title', photo.alt_description);
+        setAttributes(pic, {
+            src:photo.urls.regular,
+            alt:photo.alt_description,
+            title:photo.alt_description,
+        });
 
         // poulate to DOM
         item.appendChild(pic);
