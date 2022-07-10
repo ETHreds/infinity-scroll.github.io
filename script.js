623d76f2a1d69b5f1077abd89c9d093775e80df1
ETHreds <jcmaglaw@gmail.com>
2022-07-11 00:51:02 +0300

    INFINITY SCROLL  compare window screen and scrolled to document height  add scroll listener
     add load listener
     link to scroll litener to call function once

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const imageContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let picsLoaded = 0;
+let totalPics = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "J5lzh6qG_3gBZhlcvuU-UDk-duXJFmGrrl_aYDwWNJM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check idf pic is loaded
+function picLoaded() {
+    picsLoaded++;
+
+    if (picsLoaded === totalPics) {
+        ready = true;
+        loader.hidden = true;
+
+    }
+    
+}
 
 // Helper functon to set attributes\
 
@@ -18,12 +33,12 @@ function setAttributes(element, attributes) {
 }
 // Display photos and put into DOM
 function displayPhotos() {
+    picsLoaded = 0;
+    totalPics = photosArray.length;
     // get info in photoArray and populate
     photosArray.forEach((photo) => {
         // createthe <a> element to unsplash
         const item = document.createElement('a');
-        // item.setAttribute('href', photo.links.html);
-        // item.setAttribute('target', '_blank');
         setAttributes(item, {
             href:photo.links.html,
             target: '_blank',
@@ -32,14 +47,14 @@ function displayPhotos() {
         // image to be be the thing inside the link
 
         const pic = document.createElement('img');
-        // pic.setAttribute('src', photo.urls.regular);
-        // pic.setAttribute('alt', photo.alt_description);
-        // pic.setAttribute('title', photo.alt_description);
         setAttributes(pic, {
             src:photo.urls.regular,
             alt:photo.alt_description,
             title:photo.alt_description,
         });
+
+        // Event listener to chech hen pic are loaded
+        pic.addEventListener('load', picLoaded);
 
         // poulate to DOM
         item.appendChild(pic);
@@ -63,6 +78,19 @@ async function getPhotos() {
     }
 
 }
+
+// Listener to check for scrolled screen and next to the bottom
+window.addEventListener('scroll' ,() => {
+
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+        
+    }
+    
+});
+
+
 
 // On load
 getPhotos();
